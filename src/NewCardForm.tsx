6fa7e04f2828fdff602a616a6e4b5615eb5652f5
alucas <alicelucas93@gmail.com>
2020-12-29 15:55:00 +0100

    add field

--- src/NewCardForm.tsx
+++ src/NewCardForm.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import SimpleCard from "./SimpleCard";
+import {CardContent} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import TextField from "@material-ui/core/TextField";
 
 const NewCardForm = () => {
     return (
-        <SimpleCard title={"NEW TITLE"} authors={"NEW AUTHOR"} summary={"NEW SUMMARY"}/>
+        <Card>
+            <CardContent>
+                <form noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Standard" />
+                </form>
+
+            </CardContent>
+        </Card>
+        // <SimpleCard title={"NEW TITLE"} authors={"NEW AUTHOR"} summary={"NEW SUMMARY"}/>
     )
 }
 

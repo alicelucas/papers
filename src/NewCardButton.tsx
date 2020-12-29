749b8a8bb59639441a38feabbe7a80c7562ec9a5
alucas <alicelucas93@gmail.com>
2020-12-29 15:51:50 +0100

    show a card

--- src/NewCardButton.tsx
+++ src/NewCardButton.tsx
@@ -1,9 +1,7 @@
 import Button from "@material-ui/core/Button";
 import React from "react";
-import {Dialog, DialogContentText} from "@material-ui/core";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import DialogContent from "@material-ui/core/DialogContent";
+import {Dialog} from "@material-ui/core";
+import NewCardForm from "./NewCardForm";
 
 const NewCardButton = () => {
 
@@ -23,12 +21,7 @@ const NewCardButton = () => {
         <React.Fragment>
             <Button variant="outlined" onClick={onClick}> Add card </Button>
             <Dialog onClose={handleClose} open={open}>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous location data to
-                        Google, even when no apps are running.
-                    </DialogContentText>
-                </DialogContent>
+                <NewCardForm/>
             </Dialog>
         </React.Fragment>
 
---
+++ src/NewCardForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SimpleCard from "./SimpleCard";
+
+const NewCardForm = () => {
+    return (
+        <SimpleCard title={"NEW TITLE"} authors={"NEW AUTHOR"} summary={"NEW SUMMARY"}/>
+    )
+}
+
+export default NewCardForm;

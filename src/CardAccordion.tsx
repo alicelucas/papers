abc63fc9d09f8d27f8543f55d16fe9b7319fcb91
alucas <alicelucas93@gmail.com>
2021-03-06 13:11:14 -0500

    SectionAccordion

--- src/CardAccordion.tsx
+++ src/CardAccordion.tsx
@@ -1,13 +1,10 @@
-import React, {useState} from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {TextField} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import EditIcon from '@material-ui/icons/Edit';
+import {SectionAccordion} from "./SectionAccordion";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -24,54 +21,24 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function CardAccordion() {
     const classes = useStyles();
 
-    const [isEdit, setIsEdit] = useState<boolean>(false);
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet bla" +
+        "ndit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsi" +
+        "t amet blandit leo lobortis eget."
 
     return (
         <div className={classes.root}>
-            <Accordion>
+            <Accordion disabled>
                 <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Typography className={classes.heading}>Accordion 1</Typography>
-                </AccordionSummary>
-
-                <AccordionDetails>
-                    {!isEdit && (<Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>)}
-                    {isEdit && (  <TextField fullWidth defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget. ." id="filled-basic" label="Filled" multiline={true} variant="filled" />)}
-                    <IconButton onClick={() => {setIsEdit(!isEdit)}}>
-                        <EditIcon />
-                    </IconButton>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel2a-content"
                     id="panel2a-header"
                 >
-                    <Typography className={classes.heading}>Accordion 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <TextField fullWidth defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget." id="filled-basic" label="Filled" variant="filled" />
-                </AccordionDetails>
-            </Accordion>
-            <Accordion disabled>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel3a-content"
-                    id="panel3a-header"
-                >
-                    <Typography className={classes.heading}>Disabled Accordion</Typography>
+                    <Typography className={classes.heading}>Title, authors, journal conference, date</Typography>
                 </AccordionSummary>
             </Accordion>
+            <SectionAccordion content={text} title={"Why is this work important?"}/>
+            <SectionAccordion content={text} title={"What do they propose?"}/>
+            <SectionAccordion content={text} title={"How does it work?"}/>
+            <SectionAccordion content={text} title={"What are the results?"}/>
         </div>
     );
 }
---
+++ src/SectionAccordion.tsx
@@ -0,0 +1,55 @@
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Typography from "@material-ui/core/Typography";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import {TextField} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import React, {useState} from "react";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        input: {
+            paddingTop: 2,
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            fontWeight: theme.typography.fontWeightRegular,
+        },
+    }),
+);
+
+type SectionAccordionPropsType = {
+    title: string,
+    content: string
+}
+
+export const SectionAccordion = ( {title, content}: SectionAccordionPropsType) => {
+    const classes = useStyles();
+
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography className={classes.heading}>{title}</Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+                {!isEdit && (<Typography>
+                    {content}
+                </Typography>)}
+                {isEdit && (  <TextField InputProps={ {className: classes.input}} fullWidth defaultValue={content} id="filled-basic" multiline={true} variant="filled" />)}
+                <IconButton onClick={() => {setIsEdit(!isEdit)}}>
+                    <EditIcon />
+                </IconButton>
+            </AccordionDetails>
+        </Accordion>
+    )
+}

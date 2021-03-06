f91de1bab528c5c5e8dfd7c8fa09824c0439f6cf
alucas <alicelucas93@gmail.com>
2021-03-06 12:46:13 -0500

    Dialog absolute position

--- src/CardAccordionDialog.tsx
+++ src/CardAccordionDialog.tsx
@@ -1,13 +1,23 @@
 import {Dialog} from "@material-ui/core";
 import React from "react";
 import CardAccordion from "./CardAccordion";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles({
+    dialog: {
+        position: 'absolute',
+        top: "100px"
+    }
+});
 
 type CardAccordionDialogProps = {
     open: boolean;
 }
 export const CardAccordionDialog = ( { open } : CardAccordionDialogProps) => {
+    const classes = useStyles();
+
     return (
-        <Dialog fullWidth={true} maxWidth="lg" open={open}>
+        <Dialog classes={{paper: classes.dialog}} fullWidth={true} maxWidth="lg" open={open}>
             <CardAccordion/>
         </Dialog>
     )

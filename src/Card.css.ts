d424a3df278add103866f36e3ce5245d402a4f3c
alucas <alicelucas93@gmail.com>
2021-03-27 14:50:38 -0400

    FEATURE: CardPreview design

--- src/Card.css.ts
+++ src/Card.css.ts
@@ -2,7 +2,10 @@ import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        minWidth: 400,
+        minWidth: 350,
+    },
+    media: {
+        height: 200,
     },
     bullet: {
         display: 'inline-block',
--- src/components/CardPreview/CardPreview.tsx
+++ src/components/CardPreview/CardPreview.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import useStyles from "../../Card.css";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
-import {RemoveCardButton} from "../Buttons/RemoveCardButton";
 import {CardAccordionDialog} from "../CardAccordion/CardAccordionDialog";
 import {Card as CardPreviewType} from "../../types/Card";
 import {useDispatch, useSelector} from "react-redux";
 import {cardsSlice, updatedCardContentSelector} from "../../store/slice";
 import {Section} from "../../types/Section";
+import {CardActionArea} from "@material-ui/core";
 
 type CardPreviewsProps = {
     card: CardPreviewType
@@ -36,7 +34,7 @@ export default function CardPreview({card}: CardPreviewsProps) {
 
     const [openCardAccordionDialog, setOpenCardAccordionDialog] = React.useState<boolean>(false)
 
-    const onLearnMoreClick = () => {
+    const onCardClick = () => {
         setOpenCardAccordionDialog(true);
         dispatch(cardsSlice.actions.setSelectedCard({id: card._id}))
         dispatch(cardsSlice.actions.setSelectedSection({section: Section.Why}))
@@ -51,26 +49,29 @@ export default function CardPreview({card}: CardPreviewsProps) {
     }
 
     return (
-        <Grid key={card._id} item xs={4}>
-            <Card className={classes.root}>
-                <CardContent>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                        {[card.journal, card.date].join(", ")}
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        {card.title}
-                    </Typography>
-                    <Typography className={classes.pos} color="textSecondary">
-                        {card.authors}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button onClick={onLearnMoreClick} size="small">Learn More</Button>
-                    <CardAccordionDialog card={card} handleClose={handleCardAccordionDialogClose} open={openCardAccordionDialog}/>
-                    <RemoveCardButton handleRemoveCard={handleRemoveCard}/>
-                </CardActions>
-            </Card>
-        </Grid>
+        <React.Fragment>
+            <Grid key={card._id} item xs={4}>
+                <Card className={classes.root} onClick={onCardClick}>
+                    <CardActionArea className={classes.media}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                {[card.journal, card.date].join(", ")}
+                            </Typography>
+                            <Typography variant="h6" component="h2">
+                                {card.title}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                {card.authors}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    {/*<RemoveCardButton handleRemoveCard={handleRemoveCard}/>*/}
+                </Card>
+            </Grid>
+            <CardAccordionDialog card={card} handleClose={handleCardAccordionDialogClose} open={openCardAccordionDialog}/>
+        </React.Fragment>
+
+
 
     );
 }

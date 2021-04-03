8befbff4ba61aff15733f768e18d240a57e2cc85
alucas <alicelucas93@gmail.com>
2021-04-03 12:56:24 -0400

    style search bar

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -43,8 +35,8 @@ const useStyles = makeStyles((theme: Theme) =>
             marginLeft: 0,
             width: '100%',
             [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(3),
-                width: 'auto',
+                marginLeft: theme.spacing(23),
+                width: "30%",
             },
         },
         searchIcon: {
@@ -64,21 +56,8 @@ const useStyles = makeStyles((theme: Theme) =>
             // vertical padding + font size from searchIcon
             paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
             transition: theme.transitions.create('width'),
-            width: '100%',
             [theme.breakpoints.up('md')]: {
-                width: '20ch',
-            },
-        },
-        sectionDesktop: {
-            display: 'none',
-            [theme.breakpoints.up('md')]: {
-                display: 'flex',
-            },
-        },
-        sectionMobile: {
-            display: 'flex',
-            [theme.breakpoints.up('md')]: {
-                display: 'none',
+                width: '55ch',
             },
         },
     }),
@@ -108,10 +87,8 @@ export default function CardAppBar() {
                         />
                     </div>
                     <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                    </div>
-                    <div className={classes.sectionMobile}>
-                    </div>
+                    <div className={classes.grow} />
+                    <div className={classes.grow} />
                 </Toolbar>
             </AppBar>
         </div>

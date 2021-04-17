a2b2dc3c9e93637dc700572fa355765cde22d448
alucas <alicelucas93@gmail.com>
2021-04-17 13:16:49 -0400

    working for searching through titles

--- src/components/AppBar/SearchField/SearchField.tsx
+++ src/components/AppBar/SearchField/SearchField.tsx
@@ -1,22 +1,31 @@
 import SearchIcon from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
 import React, {ChangeEvent} from "react";
-import {useSelector} from "react-redux";
-import {cardsSelector} from "../../../store/cardsSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {cardsSelector, cardsSlice} from "../../../store/cardsSlice";
 import {useStyles} from "../AppBar.css";
+import {Card} from "../../../types/Card";
+import * as _ from "lodash"
 
 export const SearchField = () => {
     const classes = useStyles();
 
     const cards = useSelector(cardsSelector);
 
+    const dispatch = useDispatch();
+
     const handleSearch = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const query = event.target.value;
-        // const ids = _.map(cards, (card: Card) => {
-        //     return ()
-        // })
-
+        const cardsIds: Array<string> = [];
+        _.forEach(cards, (card: Card) =>
+        {
+            if ((card.title).toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                cardsIds.push(card._id)
+            }
+        })
+        dispatch(cardsSlice.actions.setVisibleCardIds({visibleCardsIds: cardsIds }))
     }
+
     return(
         <div className={classes.search}>
             <div className={classes.searchIcon}>

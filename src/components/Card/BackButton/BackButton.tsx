41140b8935ad8dcb891121801ed8caab98624b13
alucas <alicelucas93@gmail.com>
2021-04-19 09:08:31 -0400

    BackButton component

---
+++ src/components/Card/BackButton/BackButton.tsx
@@ -0,0 +1,37 @@
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import React from "react";
+import {cardsSlice, selectedSectionSelector} from "../../../store/cardsSlice";
+import {Section} from "../../../types/Section";
+import {useDispatch, useSelector} from "react-redux";
+import {useStyles} from "../Card.css";
+
+
+export const BackButton = () => {
+
+    const classes = useStyles();
+
+    const dispatch = useDispatch();
+
+    const selectedSection = useSelector(selectedSectionSelector)
+
+    const onPreviousSectionClick = () => {
+        if (selectedSection - 1 === -1) return;
+        else {
+            dispatch(cardsSlice.actions.setSelectedSection({section: (selectedSection - 1) as Section}))
+        }
+    };
+
+    if (selectedSection === 0) return (
+        <IconButton disabled className={classes.icon} onClick={onPreviousSectionClick}>
+            <ArrowBackIcon fontSize={"large"}/>
+        </IconButton>
+    )
+    else {
+        return (
+            <IconButton className={classes.icon} onClick={onPreviousSectionClick}>
+                <ArrowBackIcon fontSize={"large"}/>
+            </IconButton>)
+    }
+
+}
---
+++ src/components/Card/BackButton/index.ts
@@ -0,0 +1,1 @@
+import { BackButton } from "./BackButton";

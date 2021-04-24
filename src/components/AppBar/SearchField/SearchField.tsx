132e067b24c0ff8c38ff9aad358474b61aca6de9
alucas <alicelucas93@gmail.com>
2021-04-24 10:23:14 -0400

    closes #42 show label in search bar

--- src/components/AppBar/SearchField/SearchField.tsx
+++ src/components/AppBar/SearchField/SearchField.tsx
@@ -6,7 +6,7 @@ import {cardsSelector, cardsSlice} from "../../../store/cardsSlice";
 import {useStyles} from "../AppBar/AppBar.css";
 import {Card} from "../../../types/Card";
 import * as _ from "lodash"
-import {searchTermSelector} from "../../../store/appSlice";
+import {appSlice, searchTermSelector} from "../../../store/appSlice";
 
 export const SearchField = () => {
     const classes = useStyles();
@@ -19,10 +19,19 @@ export const SearchField = () => {
 
     const handleSearch = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const query = event.target.value;
+        dispatch(appSlice.actions.setSearchTerm({searchTerm: query}))
         const cardsIds: Array<string> = [];
+        const labelIndexLocation = query.indexOf("label:");
+        let labelTerm = "";
+        if (labelIndexLocation !== -1) {
+            labelTerm = query.slice(labelIndexLocation+6);
+        }
         _.forEach(cards, (card: Card) =>
         {
             if ((card.title).toLowerCase().indexOf(query.toLowerCase()) > -1 || (card.authors).toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                cardsIds.push(card._id)
+            }
+            if (labelIndexLocation !== -1 && labelTerm && (card.labels).map((label: string) => label.toLowerCase()).includes(labelTerm)) {
                 cardsIds.push(card._id)
             }
         })

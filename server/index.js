6824d01bbce5b110fa031c0d28865aabf7cce19a
alucas <alicelucas93@gmail.com>
2021-05-05 10:34:41 -0400

    fixes #41 #43

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -9,64 +10,45 @@ dotenv.config();
 const app = express();
 const port = process.env.EXPRESS_PORT;
 
-mongoose.connect('mongodb://localhost:27017/papers?authSource=admin', {useNewUrlParser: true, useUnifiedTopology: true, user: process.env.MONGO_USER, pass: process.env.MONGO_PASS});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Here?")
-});
-
-const cardSchema = new mongoose.Schema({ title: String, authors: String, journal: String, date: String, labels: Array, sections: {why: String, what: String, how: String, results: String} },
-    { collection : 'cards' });   // collection name
-const Cards = mongoose.model('Cards', cardSchema);
-
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 
-app.get('/', (req, res) => {
-  Cards.find( (err, cards) => {
-    console.info(cards)
-    if (err) console.info(err);
-    return res.json(cards);
-  })
+AWS.config.update({
+  region: "us-east-1",
+  accessKeyId: 'AKIAY7ZOORNZ7HKIWX5V',
+  secretAccessKey: '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',
 });
-
-app.post("/addCard", (req, res) => {
-  const id = mongoose.Types.ObjectId();
-  const newCard = {...req.body, _id: id};
-  Cards.create(newCard).then(() => {
-    return res.json({id: id})
-  }).catch((err) => {
-    return res.sendStatus(400);
-  });
-});
-
-app.post( "/replaceCard", (req, res) => {
-  Cards.findOneAndUpdate( {
-    _id: req.body._id
-  }, {sections: req.body.sections}, (err) => {
-    if (err) console.info(err);
-  } ).then( () => {
-    return res.sendStatus(200);
-  }).catch( () => {
-    return res.sendStatus(400);
-  })
-});
-
-app.delete("/removeCard/:id", (req, res) => {
-  Cards.findByIdAndDelete(req.params.id, (err, card) => {
-    if (err) console.info(err);
-    else {
-      console.info("Deleted: ", card)
-    }
-  } ).then( () => {
-    return res.sendStatus(200);
-  }).catch((err) => {
-        console.info(err);
-        return res.sendStatus(400);
-      });
-});
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+
+app.get('/', (req, res) => {
+  docClient.scan({TableName: "papers"}, (err, data) => {
+    if (err) {
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    }
+    else {
+      var items = [];
+      // print all the cards
+      data.Items.forEach(function(card) {
+        items.push({...card, _id: uuidv4()})
+      });
+      res.send(items)
+    }
+  });
+});
+
+// app.post("/addCard", (req, res) => {
+
+// });
+//
+// app.post( "/replaceCard", (req, res) => {
+
+// });
+//
+// app.delete("/removeCard/:id", (req, res) => {
+
+// });

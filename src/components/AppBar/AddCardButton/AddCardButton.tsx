e740ce33e127508584799415a9bb1a567537802d
alucas <alicelucas93@gmail.com>
2021-05-08 14:40:13 -0400

    typography correction

--- src/components/AppBar/AddCardButton/AddCardButton.tsx
+++ src/components/AppBar/AddCardButton/AddCardButton.tsx
@@ -19,7 +19,7 @@ export const AddCardButton = () => {
     }
     return (
         <React.Fragment>
-            <Button onClick={onClick} color="inherit"><Typography className={classes.title} >Add Card</Typography></Button>
+            <Button onClick={onClick} color="inherit"><Typography className={classes.title} >Add Paper</Typography></Button>
             <Dialog fullWidth={true} maxWidth="lg" onClose={onClose} open={open} >
                 <CardForm handleClose={onClose}/>
             </Dialog>

027fc483c46b5eab4b45dd04de78062ca95331c5
alucas <alicelucas93@gmail.com>
2021-05-15 13:42:20 -0400

    initial updateCard request

--- server/index.js
+++ server/index.js
@@ -53,10 +53,19 @@ app.post("/addCard", (req, res) => {
   })
 });
 
-
-// app.post( "/replaceCard", (req, res) => {
+// app.post("/updatedCard", (req, res) => {
+//   var title = req.body._id;
+//   const params = {
+//     TableName: "papers", Key: {title: title}, ......
+//   }
+//   docClient.update(params, (err, data) => {
+//     if (err) {
+//       console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//       console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+//     }
+//   })
 // });
-//
 
 app.delete("/removeCard/", (req, res) => {
   var title = req.body.title;

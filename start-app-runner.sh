e00ac2562d5ed945ebf789a1c37bf4cac0db5da7
alucas <alicelucas93@gmail.com>
2021-06-01 20:46:23 -0400

    chore: install pm2

--- start-app-runner.sh
+++ start-app-runner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm i pm2 -g && /usr/local/lib/nodejs/node-v12.21.0-linux-x64/bin/pm2 start server/index.js && /usr/local/lib/nodejs/node-v12.21.0-linux-x64/bin/pm2 serve build/ 80 --spa
+npm i pm2 -g && /usr/local/lib/nodejs/node-v12.21.0-linux-x64/bin/pm2 start server/index.js && /usr/local/lib/nodejs/node-v12.21.0-linux-x64/bin/pm2 serve build/ 80 --spa && watch -n 30 /usr/local/lib/nodejs/node-v12.21.0-linux-x64/bin/pm2 status
